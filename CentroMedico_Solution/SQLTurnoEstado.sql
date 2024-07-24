d87268cc22c3fecf017fa3cb1a04a8c705d5a66a
Grym1314 <chucho_031199@hotmail.com>
2024-07-24 16:38:58 -0600

    Clausula TOP #13 07/24/24

---
+++ CentroMedico_Solution/SQLTurnoEstado.sql
@@ -0,0 +1,6 @@
+
+
+SELECT * FROM TurnoEstado
+
+INSERT INTO TurnoEstado VALUES ('Realizado'), ('Cancelado'),('Rechazado'), ('Realizado'), ('Postergado')
+,('Anulado'), ('Derivado')
---
+++ SQLClausleTOP.sql
@@ -0,0 +1,4 @@
+
+SELECT * FROM paciente
+SELECT TOP 1 * FROM paciente --Manda a llamar solo una cantidad especifica de registros
+SELECT TOP 1 nombre,apellido FROM paciente
