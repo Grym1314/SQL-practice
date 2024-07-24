11b5017a11cdbaf4d268f4b901b743487af09d45
Grym1314 <chucho_031199@hotmail.com>
2024-07-24 13:56:11 -0600

    How to INSERT one value #11 07/24/24

---
+++ SQLInsertOneValue.sql
@@ -0,0 +1,5 @@
+
+SELECT * FROM paciente
+
+INSERT INTO paciente (dni,nombre, apellido,fNacimiento, domicilio,idPais, telefono,email,observacion)
+VALUES ('333522349', 'Mosha','Contreras','1974-11-02','Cuajimalpa 05200','ARG',NULL,'mosga@gmail.com','Sin Observaciones' )
