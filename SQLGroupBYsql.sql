0b5c1ae5c49c8720514d2c88e3f6e2a9d4787683
Grym1314 <chucho_031199@hotmail.com>
2024-07-24 16:58:56 -0600

    Clausula WHERE y GROUP BY #16 07/24/24

---
+++ SQLGroupBYsql.sql
@@ -0,0 +1,2 @@
+
+SELECT idPais FROM paciente GROUP BY idPais --Nos permite ejecutar y saber para calcular promedios
---
+++ SQLWHEREsql.sql
@@ -0,0 +1,2 @@
+
+SELECT * FROM paciente WHERE idPais = 'MEX'--Saldra la cantidad de paciente con MEX nacionalidad
