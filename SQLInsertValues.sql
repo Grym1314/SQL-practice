a1ac825cc80d7f1bea13989ecca17fe0227844d7
Grym1314 <chucho_031199@hotmail.com>
2024-07-24 13:56:58 -0600

    How to INSERT values #12 07/24/24

---
+++ SQLInsertValues.sql
@@ -0,0 +1,6 @@
+
+SELECT * FROM paciente
+
+INSERT INTO paciente (dni,nombre, apellido,fNacimiento, domicilio,idPais, telefono,email,observacion)
+VALUES ('333522349', 'Mosha','Contreras','1974-11-02','Cuajimalpa 05200','ARG',NULL,'mosga@gmail.com','Sin Observaciones' )
+,('3331234', 'Erik','Martin','1999-11-10','Cuaji','COL',NULL,'erikga@gmail.com',NULL )
