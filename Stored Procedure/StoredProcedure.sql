fabd3d1a7b3a3adff370edb09ce96744f5e55f29
Grym1314 <chucho_031199@hotmail.com>
2024-08-01 11:14:35 -0600

    Stored Procedure first example #32 08/01/24

--- Stored Procedure/StoredProcedure.sql
+++ Stored Procedure/StoredProcedure.sql
@@ -1,2 +1,13 @@
 --Store Procedure
 --Conjunto de instrucciones, almacenado en SQL server y se ejecurata cuando lo llames
+--Dentro del parentesis se coloca los parametros que se van a procesar
+CREATE PROC S_PrimeroPaciente (
+	@idpaciente int
+) 
+AS-- todos los scripts que queramos ejecutar dentro nuestro procedimientos
+
+SELECT * FROM paciente WHERE idPaciente = @idpaciente --Parametro entrada
+
+GO-- hace que continue corriendo otros parametros, si no lo pones y colocas otro SP saldria ERROR
+
+5
