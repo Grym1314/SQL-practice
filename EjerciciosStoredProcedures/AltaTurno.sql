c7f6dd875c2e4608fbea39384fa966496fb4f4ab
Grym1314 <chucho_031199@hotmail.com>
2024-08-05 15:58:29 -0600

    EXERCISE Insert Turno #50 08/05/24

---
+++ EjerciciosStoredProcedures/AltaTurno.sql
@@ -0,0 +1,29 @@
+
+ALTER PROC Alta_Turnos( 
+	@fechaturno CHAR(14),--20190215 12:00
+	@observacion observacion=' ',
+	@idPaciente paciente,
+	@idMedico medico
+	)
+AS
+IF NOT EXISTS(SELECT TOP 1 idTurno FROM Turno WHERE fechaTurno=@fechaturno)
+	BEGIN
+		INSERT INTO Turno(fechaTurno,idEstado,observacion)
+		VALUES (@fechaturno,0,@observacion)
+
+		DECLARE @auxIdTurno turno
+		SET @auxIdTurno = @@IDENTITY--Ultimo valor insertado dentro de un campo con el valor IDENTITY ON
+
+		INSERT INTO turnoPaciente(idTurno,idPaciente,idMedico)
+		VALUES (@auxIdTurno,@idPaciente,@idMedico)
+
+		PRINT 'El turno ha sido registrado'
+		RETURN
+	END
+ELSE
+	BEGIN
+		PRINT 'El turno ya existe'
+		RETURN
+	END
+
+
