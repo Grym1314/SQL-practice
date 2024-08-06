71c21bbb8d0848f53412994dab15d600ff477106
Grym1314 <chucho_031199@hotmail.com>
2024-08-06 16:09:55 -0600

    Sentencias para fechas DATES #63 08/06/24

---
+++ Sentencias/GETDATE.sql
@@ -0,0 +1,18 @@
+--GETDATE y GETUTCFATE
+
+SELECT GETDATE()
+SELECT GETUTCDATE()
+
+--DATEADD
+SELECT DATEADD(Day,2,GETDATE())
+SELECT DATEADD(Day,-2,GETDATE())
+
+--DATEIFF saber la cantidad de fecha de distancia en este ejemplo es del 2017 al año presente
+SELECT DATEDIFF(YEAR,GETDATE(),'20170210')
+
+--DATEPART
+SELECT DATEPART (dw,GETDATE())
+
+--ISDATE-Saber si es una fecha o no
+--Da boolean TRUE=1 FALSE=0
+PRINT ISDATE('DFASFSD')
