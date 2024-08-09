de33699ee66f64c581c99ef6adcbd6b32b233380
Grym1314 <chucho_031199@hotmail.com>
2024-08-08 21:34:24 -0600

    Cast and Convert #66 08/08/24

--- Sentencias/CastandConvert.sql
+++ Sentencias/CastandConvert.sql
@@ -1,3 +1,11 @@
 --CAST: La funcion CAST es generica y se encarga de convertirnos un dato en otro
 
---COVERT: Tiene otros parametros a diferencia de CAST, nos permitira convertir los tipos de datos
+--COVERT: Convierte a un tipo de dato especifico el valor de un campo o variable
+
+
+DECLARE @numero MONEY
+SET @numero=500.40
+PRINT @numero
+--SELECT CAST (@numero AS INT) AS Numero 
+
+SELECT CONVERT(int,@numero)
