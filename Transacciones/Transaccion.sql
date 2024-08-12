63293d4ea83a850c7e4191115ff88b09cdddc815
Grym1314 <chucho_031199@hotmail.com>
2024-08-11 19:29:29 -0600

    TRANSACCION BEGIN #68 08/11/24

--- Transacciones/Transaccion.sql
+++ Transacciones/Transaccion.sql
@@ -1,1 +1,15 @@
 --Transaccion
+--Manipulacion de datos
+
+SELECT * FROM paciente
+
+BEGIN TRAN 
+	UPDATE paciente	SET apellido='Flores' WHERE idPaciente=8
+IF @@ROWCOUNT = 1 --Nos señala cuantos registros fueron editados, borrados o consulados
+	BEGIN
+		COMMIT TRAN
+	END
+ELSE
+	BEGIN 
+	ROLLBACK TRAN--Cancela el commit del transaction
+	END
