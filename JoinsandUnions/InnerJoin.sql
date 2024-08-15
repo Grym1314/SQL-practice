8a49016285603189a66273a733e02be351c37a8f
Grym1314 <chucho_031199@hotmail.com>
2024-08-14 20:46:01 -0600

    Inner Join #71 08/14/24

--- JoinsandUnions/InnerJoin.sql
+++ JoinsandUnions/InnerJoin.sql
@@ -1,1 +1,8 @@
 --Iner Join
+--Se reduce la consulta a los campos de las tablas que tengan misma informacion
+SELECT * FROM paciente
+SELECT * FROM turnoPaciente
+
+SELECT * FROM paciente P-- la P es un apodo al igual que la T 
+INNER JOIN turnoPaciente T
+ON T.idPaciente = P.idPaciente
