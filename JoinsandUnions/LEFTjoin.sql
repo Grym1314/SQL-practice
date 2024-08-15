d80df4836e03aa1d5511f56f294d21f0499eed88
Grym1314 <chucho_031199@hotmail.com>
2024-08-14 20:52:28 -0600

    Left Join #73 08/14/24

--- JoinsandUnions/LEFTjoin.sql
+++ JoinsandUnions/LEFTjoin.sql
@@ -1,8 +1,9 @@
 --Left Join
+--Regresa todos los registros, y aparecera NULL aquellos los cuales no tenga registros
 
 SELECT * FROM paciente
 SELECT * FROM turnoPaciente
 
 SELECT * FROM paciente P-- la P es un apodo al igual que la T 
-INNER JOIN turnoPaciente T
+LEFT JOIN turnoPaciente T
 ON T.idPaciente = P.idPaciente
