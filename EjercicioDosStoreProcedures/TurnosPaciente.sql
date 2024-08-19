4ae77ebc432f0f38559b61098268a166bf47de88
Grym1314 <chucho_031199@hotmail.com>
2024-08-19 14:17:57 -0600

    SecondExcercise StoreProcedure TurnoPaciente 2 #81 08/19/24

--- EjercicioDosStoreProcedures/TurnosPaciente.sql
+++ EjercicioDosStoreProcedures/TurnosPaciente.sql
@@ -1,1 +1,15 @@
---Store Procedure 
+--Store Procedure Turno Paciente
+
+CREATE PROC SELECT_TurnoPaciente(
+	@idpaciente paciente
+	)
+
+AS
+SET NOCOUNT ON
+
+SELECT * FROM Turno T
+INNER JOIN turnoPaciente TP--Podemos utilizar tanto necesitemos
+ON TP.idTurno = T.idTurno
+AND TP.idPaciente = @idpaciente
+INNER JOIN paciente P
+ON P.idPaciente=TP.idpaciente
