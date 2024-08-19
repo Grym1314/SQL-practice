5b407cd28bc8feac37262df2bb2ed608a89675ff
Grym1314 <chucho_031199@hotmail.com>
2024-08-19 15:03:39 -0600

    SecondExcercise StoreProcedure TurnoPaciente practice #82 08/19/24

--- EjercicioDosStoreProcedures/TurnosPaciente.sql
+++ EjercicioDosStoreProcedures/TurnosPaciente.sql
@@ -1,5 +1,7 @@
 --Store Procedure Turno Paciente
 
+
+
 CREATE PROC SELECT_TurnoPaciente(
 	@idpaciente paciente
 	)
@@ -7,9 +9,32 @@ CREATE PROC SELECT_TurnoPaciente(
 AS
 SET NOCOUNT ON
 
-SELECT * FROM Turno T
-INNER JOIN turnoPaciente TP--Podemos utilizar tanto necesitemos
+SELECT * FROM paciente P
+INNER JOIN turnoPaciente TP
+ON TP.idPaciente = P.idPaciente
+INNER JOIN Turno T
+ON T.idTurno = TP.idTurno
+INNER JOIN MedicoEspecialidad M
+ON M.idMedico = TP.idMedico
+WHERE P.idPaciente = @idpaciente
+
+
+
+
+/*INNER JOIN turnoPaciente TP--Podemos utilizar tanto necesitemos
 ON TP.idTurno = T.idTurno
 AND TP.idPaciente = @idpaciente
 INNER JOIN paciente P
 ON P.idPaciente=TP.idpaciente
+INSERT INTO MedicoEspecialidad VALUES (1,2,'Medico Clinico')
+INSERT INTO turnoPaciente VALUES (9,1,6)
+INSERT INTO Turno VALUES ('20240818',6,'El medico esta de vacaciones')
+*/
+
+EXEC SELECT_TurnoPaciente 1
+SELECT * FROM paciente
+SELECT * FROM turnoPaciente
+SELECT * FROM Turno
+SELECT * FROM TurnoEstado
+SELECT * FROM Medico
+SELECT * FROM MedicoEspecialidad
