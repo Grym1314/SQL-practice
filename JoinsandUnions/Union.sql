e91c0528d5b2150f1637d2d044fd04f5a217b7b0
Grym1314 <chucho_031199@hotmail.com>
2024-08-18 22:36:21 -0600

    Union all #79 08/18/24

--- JoinsandUnions/Union.sql
+++ JoinsandUnions/Union.sql
@@ -1,5 +1,5 @@
 --Diferencia entre la clausula INNER con la clausula UNION
---UNION tienen que ser de los mismo tipos de datos y de la misma tabla
+--UNION tienen que ser de los mismo tipos de datos 
 SELECT * FROM Turno WHERE idEstado=3
 UNION
 SELECT * FROM Turno WHERE idEstado=1
