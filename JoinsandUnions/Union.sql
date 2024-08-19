2ef821790f5731035e4c7822e4c20913485b06ef
Grym1314 <chucho_031199@hotmail.com>
2024-08-18 22:33:33 -0600

    Union #78 08/18/24

--- JoinsandUnions/Union.sql
+++ JoinsandUnions/Union.sql
@@ -1,1 +1,5 @@
---Diferencia entre la clausula INNER con la clausula UNION, UNION nos permite unir dos consultas diferentes 
+--Diferencia entre la clausula INNER con la clausula UNION
+--UNION tienen que ser de los mismo tipos de datos y de la misma tabla
+SELECT * FROM Turno WHERE idEstado=3
+UNION
+SELECT * FROM Turno WHERE idEstado=1
