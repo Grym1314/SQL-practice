79d07685b842abafe4e4d9360e9afb0614ac5fdc
Grym1314 <chucho_031199@hotmail.com>
2024-08-19 13:44:04 -0600

    SecondExcercise StoreProcedure TurnoPaciente #80 08/19/24

--- JoinsandUnions/Union.sql
+++ JoinsandUnions/Union.sql
@@ -1,5 +1,5 @@
 --Diferencia entre la clausula INNER con la clausula UNION
 --UNION tienen que ser de los mismo tipos de datos 
 SELECT * FROM Turno WHERE idEstado=3
-UNION
+UNION-- Elimina las repiticiones/ UNION ALL devuelve todos hasta las repeticiones 
 SELECT * FROM Turno WHERE idEstado=1
