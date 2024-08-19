c8e6ed510db1a37be5575117aeed7c383d3aa5d5
Grym1314 <chucho_031199@hotmail.com>
2024-08-19 17:34:34 -0600

    SecondExcercise StoreProcedure HisotriaPaciente Practice #85 08/19/24

--- EjercicioDosStoreProcedures/HistoriaPaciente.sql
+++ EjercicioDosStoreProcedures/HistoriaPaciente.sql
@@ -4,13 +4,26 @@ CREATE PROC SEL_HistoriaPaciente(
 AS
 SET NOCOUNT ON
 
-SELECT * FROM paciente P
-INNER JOIN HistoriaPaciente HP
-ON HP.idPaciente = P.idPaciente
-INNER JOIN historia H
-ON H.idHistoria = HP.idHistoria
-INNER JOIN MedicoEspecialidad ME
-ON ME.idMedico = HP.idMedico
-INNER JOIN Medico M
-ON M.idMedico = ME.idMedico
-WHERE P.idPaciente = @idpaciente
+	SELECT * FROM paciente P
+	INNER JOIN HistoriaPaciente HP
+	ON HP.idPaciente = P.idPaciente
+	INNER JOIN historia H
+	ON H.idHistoria = HP.idHistoria
+	INNER JOIN MedicoEspecialidad ME
+	ON ME.idMedico = HP.idMedico
+	INNER JOIN Medico M
+	ON M.idMedico = ME.idMedico
+	WHERE P.idPaciente = @idpaciente
+
+--Pruebas y ejecuciones
+EXEC SEL_HistoriaPaciente 6
+
+SELECT * FROM historia
+SELECT * FROM HistoriaPaciente
+SELECT * FROM Medico
+SELECT * FROM MedicoEspecialidad
+SELECT * FROM Especialidad
+
+INSERT INTO historia VALUES ('20241020','Observaciones Prueba Dos')
+INSERT INTO HistoriaPaciente VALUES (1,6,2),(2,4,4)
+INSERT INTO MedicoEspecialidad VALUES (2,4,'Jeda de especialidad'),(4,3,'Medico Residente')
