b016490b5502df138c9743b912d5d205b644a9b4
Grym1314 <chucho_031199@hotmail.com>
2024-10-19 20:08:21 -0600

    Stored Procedure-Update turn FINISHED #81 10/19/24

--- Stored Procedure/UPT_Turn.sql
+++ Stored Procedure/UPT_Turn.sql
@@ -1,10 +1,25 @@
 --Update turn
 
 /*
-	SELECT * FROM Turno 
+	SELECT * FROM Turno
+	SELECT * FROM turnoPaciente
 */
 
-CREATE PROC UPD_Turno()
+EXEC  UPD_Turno 4,1,'El paciente cancelo'
+
+ALTER PROC UPD_Turno(
+	@idTurno turno,
+	@estado tinyint,
+	@observacion observacion
+	)
 
 AS
 SET NOCOUNT ON
+
+IF exists(SELECT * FROM Turno 
+	WHERE idTurno = @idTurno)
+		UPDATE Turno SET idEstado = @estado,
+						observacion = @observacion
+		WHERE idTurno = @idTurno--Es importante poner el WHERE porque podemos actualizar y perder todos los datos
+ELSE
+	SELECT 0 AS resultado
